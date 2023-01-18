73263d4776c561aa9880cb83da2ff78977dd79ec
SadRus <rystixx@yandex.ru>
2023-01-19 01:05:53 +0300

    reduce db queries number.

--- blog/views.py
+++ blog/views.py
@@ -19,6 +19,7 @@ def serialize_post(post):
 
 
 def serialize_post_optimized(post):
+    tags_for_post = post.tags.annotate(posts_count=Count('posts'))
     return {
         'title': post.title,
         'teaser_text': post.text[:200],
@@ -27,9 +28,13 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [
+            serialize_tag_optimized(tag) for tag in tags_for_post
+        ],
+        # 'tags': post.tags.all(),
         'first_tag_title': post.tags.all()[0].title,
     }
+
 
 
 def serialize_tag(tag):
@@ -38,6 +43,11 @@ def serialize_tag(tag):
         'posts_with_tag': len(Post.objects.filter(tags=tag)),
     }
 
+def serialize_tag_optimized(tag):
+    return {
+        'title': tag.title,
+        'posts_with_tag': tag.posts_count,
+    }
 
 def index(request):
     most_popular_posts = Post.objects.popular()[:5] \
@@ -57,7 +67,9 @@ def index(request):
         'page_posts': [
             serialize_post_optimized(post) for post in most_fresh_posts
         ],
-        'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
+        'popular_tags': [
+            serialize_tag_optimized(tag) for tag in most_popular_tags
+        ],
     }
     return render(request, 'index.html', context)
 
